465b10410482fcc41e225b25f6f203e60ddcfc58
Grant-S-Clark <grantsamuelclark@gmail.com>
2025-02-17 10:27:45 -0500

    no msg

--- mysite/appdir/__init__.py
+++ mysite/appdir/__init__.py
@@ -2,4 +2,5 @@
 
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = "SECRETKEYTEMPORARYSTRING"
 from appdir import routes
--- mysite/appdir/routes.py
+++ mysite/appdir/routes.py
@@ -1,9 +1,13 @@
 # File: routes.py
 
-from flask import render_template
+from flask import render_template, session
 from appdir import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", user="Grant")
+    if 'user' not in session:
+        return render_template("login.html")
+    else:
+        return render_template("index.html", user=session['user'])
+
